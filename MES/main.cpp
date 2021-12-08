362e1d963afbc0bef4da0d0bef733470f6368ee6
antooni <antoni.pawlak@protonmail.com>
2021-12-08 14:10:28 +0100

    multiple iterations work

--- MES/main.cpp
+++ MES/main.cpp
@@ -11,6 +11,8 @@
 
 const double eps = 1e-12;
 
+const double DELTA_T = 1.0;
+
 double fun(double x) {return 5 * x * x + 3 * x + 6;}
 
 double fun2(double x, double y) {return 5 * x * x * y * y + 3 * x * y + 6;}
@@ -25,95 +27,85 @@ int main()
 {
 	Element4* element = new Element4(DWU_PUNKTOWY);
 
-	Grid* g = new Grid(0.1, 0.1, 4, 4);
+	Grid* g = new Grid(0.1, 0.1, 31, 31);
 
-	g->homework();
+	//g->homework();
 
 	const int ILOSC_WEZLOW = g->nodes.size();
 	
-	Matrix* HEX = new Matrix(ILOSC_WEZLOW, ILOSC_WEZLOW, 0.0);
-	Matrix* CEX = new Matrix(ILOSC_WEZLOW, ILOSC_WEZLOW, 0.0);
-	Matrix* PEX = new Matrix(1, ILOSC_WEZLOW, 0.0);
-
-	for (int nrElementu = 0; nrElementu < g->elements.size(); nrElementu++) {
-		H* h = new H(nrElementu, *element, *g);
-		Hbc* hbc = new Hbc(nrElementu, *element, *g);
-		P* p = new P(nrElementu, *element, *g);
-		C* c = new C(nrElementu, *element, *g);
-
-		Matrix res = h->macierz->add(hbc->macierz);
 
 
-		for (int i = 0; i < res.mRzedow; i++) {
-			for (int j = 0; j < res.nKolumn; j++) {
-				HEX->A[g->elements[nrElementu]->nodesID[i] - 1][g->elements[nrElementu]->nodesID[j] - 1] += res.A[i][j];
+	for (double iteracja = 0.0; iteracja < 2.0; iteracja += 1.0) {
+		Matrix* HEX = new Matrix(ILOSC_WEZLOW, ILOSC_WEZLOW, 0.0);
+		Matrix* CEX = new Matrix(ILOSC_WEZLOW, ILOSC_WEZLOW, 0.0);
+		Matrix* PEX = new Matrix(1, ILOSC_WEZLOW, 0.0);
+
+		for (int nrElementu = 0; nrElementu < g->elements.size(); nrElementu++) {
+			H* h = new H(nrElementu, *element, *g);
+			Hbc* hbc = new Hbc(nrElementu, *element, *g);
+			P* p = new P(nrElementu, *element, *g);
+			C* c = new C(nrElementu, *element, *g);
+
+			Matrix res = h->macierz->add(hbc->macierz);
+
+
+			for (int i = 0; i < res.mRzedow; i++) {
+				for (int j = 0; j < res.nKolumn; j++) {
+					HEX->A[g->elements[nrElementu]->nodesID[i] - 1][g->elements[nrElementu]->nodesID[j] - 1] += res.A[i][j];
+				}
 			}
+
+			for (int i = 0; i < res.mRzedow; i++) {
+				for (int j = 0; j < res.nKolumn; j++) {
+					CEX->A[g->elements[nrElementu]->nodesID[i] - 1][g->elements[nrElementu]->nodesID[j] - 1] += c->macierz->A[i][j];
+				}
+			}
+
+			for (int j = 0; j < p->macierz->mRzedow; j++) {
+				PEX->A[g->elements[nrElementu]->nodesID[j] - 1][0] += p->macierz->A[j][0];
+			}
+
 		}
 
-		for (int i = 0; i < res.mRzedow; i++) {
-			for (int j = 0; j < res.nKolumn; j++) {
-				CEX->A[g->elements[nrElementu]->nodesID[i] - 1][g->elements[nrElementu]->nodesID[j] - 1] += c->macierz->A[i][j];
+		Matrix* RES = new Matrix(ILOSC_WEZLOW, ILOSC_WEZLOW, 0.0);
+
+		for (int i = 0; i < ILOSC_WEZLOW; i++)
+			for (int j = 0; j < ILOSC_WEZLOW; j++) {
+
+				RES->A[i][j] = HEX->A[i][j] + (CEX->A[i][j] / DELTA_T);
 			}
+
+		//RES->print();
+
+		Matrix* PES = new Matrix(1, ILOSC_WEZLOW, 0.0);
+
+		for (int i = 0; i < ILOSC_WEZLOW; i++)
+			for (int j = 0; j < ILOSC_WEZLOW; j++) {
+				double a = g->nodes[j]->t0;
+				double b = (CEX->A[i][j] / DELTA_T);
+				double c = HEX->A[i][j] + (CEX->A[i][j] / DELTA_T);
+				PES->A[i][0] += g->nodes[j]->t0 * (CEX->A[i][j] / DELTA_T);
+			}
+
+		for (int i = 0; i < ILOSC_WEZLOW; i++) {
+			PES->A[i][0] += PEX->A[i][0];
 		}
 
-		for (int j = 0; j < p->macierz->mRzedow; j++) {
-			PEX->A[g->elements[nrElementu]->nodesID[j] - 1][0] += p->macierz->A[j][0];
+		//PES->print();
+		std::cout << "ITERACJA " << iteracja << std::endl;
+		Matrix* X = gauss(RES, PES);
+		X->print();
+
+		for (int i = 0; i < ILOSC_WEZLOW; i++) {
+			double a = X->A[i][0];
+			g->nodes[i]->t0 = X->A[i][0];
 		}
 
 	}
 
-	Matrix* RES = new Matrix(ILOSC_WEZLOW, ILOSC_WEZLOW, 0.0);
-
-	for(int i = 0; i < ILOSC_WEZLOW; i++)
-		for (int j = 0; j < ILOSC_WEZLOW; j++) {
-			
-			RES->A[i][j] = HEX->A[i][j] + (CEX->A[i][j] / 50.0);
-		}
-
-	RES->print();
-
-
-	Matrix* PES = new Matrix(1, ILOSC_WEZLOW, 0.0);
-
-	for (int i = 0; i < ILOSC_WEZLOW; i++)
-		for (int j = 0; j < ILOSC_WEZLOW; j++) {
-			double a = g->nodes[j]->t0;
-			double b = (CEX->A[i][j] / 50.0);
-			double c = HEX->A[i][j] + (CEX->A[i][j] / 50.0);
-			PES->A[i][0] += g->nodes[j]->t0 * (CEX->A[i][j] / 50.0);
-		}
-
-	for (int i = 0; i < ILOSC_WEZLOW; i++) {
-		PES->A[i][0] += PEX->A[i][0];
-	}
-
-	PES->print();
-
-	gauss(RES, PES)->print();
-
-
-	//CEX->print();
-
-
-	//Matrix* AB = new Matrix(ILOSC_WEZLOW + 1, ILOSC_WEZLOW, 0.0);
-
-	//for (int i = 0; i < AB->nKolumn; i++) {
-	//	for (int j = 0; j < AB->mRzedow; j++) {
-	//		if (i == 16) AB->A[j][i] = PEX->A[j][0];
-	//		else AB->A[i][j] = HEX->A[i][j];
-	//	}
+	//for (int i = 0; i < ILOSC_WEZLOW; i++) {
+	//	std::cout << g->nodes[i]->t0 << std::endl;
 	//}
-	//AB->print();
-
-	//double* X = new double[ILOSC_WEZLOW];
-	//if (gauss(ILOSC_WEZLOW, AB->A, X))
-	//{
-	//	for (int i = 0; i < ILOSC_WEZLOW; i++)
-	//		std::cout << "x" << i + 1 << " = "  << X[i]
-	//		<< std::endl;
-	//}
-	//else
-	//	std::cout << "DZIELNIK ZERO\n";
 
 
 	return 0;
@@ -127,12 +119,10 @@ Matrix* gauss( Matrix* A, Matrix* B)
 
 	for (int i = 0; i < AB->nKolumn; i++) {
 		for (int j = 0; j < AB->mRzedow; j++) {
-			if (i == 16) AB->A[j][i] = B->A[j][0];
+			if (i == AB->nKolumn-1) AB->A[j][i] = B->A[j][0];
 			else AB->A[i][j] = A->A[i][j];
 		}
 	}
-	AB->print();
-
 
 	int i, j, k;
 	double m, s;
